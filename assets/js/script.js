d3b59d4c63fd38d0117e126b12168b0af8ce5fcc
Jason Wesley Smith <95160092+Jasonwesleysmith@users.noreply.github.com>
2022-02-06 20:14:26 -0600

    added js variables

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var searchHistory = []
+var weatherCall = "https://api.openweathermap.org/"
+var apiKey = "1382162e354e1320109c2cb84c66834d"
+var searchInput = document.querySelector("#search-input")
+var searchForm =
